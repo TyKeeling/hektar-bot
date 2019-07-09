2106de8d2ed141ab9a93a1219c4b9f35ae53798d
Tyler Keeling <tylerwkeeling@gmail.com>
2019-07-09 20:48:23 +0000

    new message type. Broken.

--- hektar/scripts/analog_servo.py
+++ hektar/scripts/analog_servo.py
@@ -3,15 +3,15 @@ import rospy
 from hektar.msg import armCtrl, armPos
 from rosserial_arduino.msg import Adc
 
-pub = rospy.Publisher('servo', armCtrl, queue_size=10)
+pub = rospy.Publisher('arm_commands', armCtrl, queue_size=10)
 
 def callback(data):
-    rospy.loginfo("received adc5: %d" % (data.adc5))
+    rospy.loginfo("received elbowPos: %d" % (data.elbowPos))
     claw_output(data.adc5)
 
 def control():
    rospy.init_node('arm_controller', anonymous=True)
-   rospy.Subscriber("adc", Adc, callback, queue_size=1, tcp_nodelay=False)
+   rospy.Subscriber("arm_positions", armPos, callback, queue_size=1, tcp_nodelay=False)
    rospy.spin()
 
 def claw_output(adcVal):
