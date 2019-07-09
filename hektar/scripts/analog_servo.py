5aa62db43e69915f2f28454139ea2b35c9543364
Tyler Keeling <tylerwkeeling@gmail.com>
2019-07-09 20:18:27 +0000

    Servo can be controlled through ROS node. Issues if stm32 publishes at high frequencies. <=100Hz tested and seems to work well with no buffer fill.

--- hektar/scripts/analog_servo.py
+++ hektar/scripts/analog_servo.py
@@ -7,16 +7,17 @@ pub = rospy.Publisher('servo', armCtrl, queue_size=10)
 
 def callback(data):
     rospy.loginfo("received adc5: %d" % (data.adc5))
-    claw_output()
+    claw_output(data.adc5)
 
 def control():
    rospy.init_node('arm_controller', anonymous=True)
-   rospy.Subscriber("adc", Adc, callback)
+   rospy.Subscriber("adc", Adc, callback, queue_size=1, tcp_nodelay=False)
    rospy.spin()
 
-def claw_output():
+def claw_output(adcVal):
    publishee = armCtrl()
-   publishee.l_claw = 120
+   publishee.l_claw = (180*adcVal//1024)
+   rospy.loginfo("l_claw: %d" % publishee.l_claw)
    pub.publish(publishee)
 
 if __name__ == '__main__':
--- hektar/scripts/talker.py
+++ hektar/scripts/talker.py
@@ -7,9 +7,11 @@ def talker():
     pub = rospy.Publisher('adc', Adc, queue_size=10)
     rospy.init_node('talker', anonymous=True)
     rate = rospy.Rate(10) # 10hz
+    i = 0
     while not rospy.is_shutdown():
         hello_str = Adc()
-        hello_str.adc5 = 105
+        hello_str.adc5 = i % 1024
+        i += 10
         pub.publish(hello_str)
         rate.sleep()
 
