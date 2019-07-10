3e7322ba9a8f6ba0a728cbe34914bf1df4d47eb4
Tyler Keeling <tylerwkeeling@gmail.com>
2019-07-10 02:18:27 +0000

    velocy control works! Velocity mesasge is relayed from the PI and is converted to PWM onthe BP.

--- hektar/scripts/analog_servo.py
+++ hektar/scripts/analog_servo.py
@@ -1,24 +1,27 @@
 #!/usr/bin/env python
 import rospy
 from hektar.msg import armCtrl, armPos
-from rosserial_arduino.msg import Adc
 
 pub = rospy.Publisher('arm_commands', armCtrl, queue_size=10)
 
 def callback(data):
-    rospy.loginfo("received elbowPos: %d" % (data.elbowPos))
-    claw_output(data.adc5)
+    rospy.loginfo("IN: elbowPos: %d" % (data.elbowPos))
+    rospy.loginfo("IN: test: %d" % (data.basePos))
+    claw_output(data)
 
 def control():
    rospy.init_node('arm_controller', anonymous=True)
    rospy.Subscriber("arm_positions", armPos, callback, queue_size=1, tcp_nodelay=False)
    rospy.spin()
 
-def claw_output(adcVal):
-   publishee = armCtrl()
-   publishee.l_claw = (180*adcVal//1024)
-   rospy.loginfo("l_claw: %d" % publishee.l_claw)
-   pub.publish(publishee)
+def claw_output(armPos):
+   output = armCtrl()
+   output.l_claw = (180*armPos.elbowPos//1024)
+   output.elbowVel = (armPos.elbowPos - 512)/4
+   rospy.loginfo("OUT: l_claw: %d" % output.l_claw)
+   rospy.loginfo("OUT: elbowVel: %d" % output.elbowVel)
+   pub.publish(output)
+   #rospy.sleep(1)
 
 if __name__ == '__main__':
     try:
