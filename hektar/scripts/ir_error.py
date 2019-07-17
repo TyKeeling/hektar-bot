724464a32e1e70715997b7fbf69bd2be6bd1da7c
Tyler Keeling <tylerwkeeling@gmail.com>
2019-07-17 10:43:28 -0700

    [minor] PID motor control nodes setup. Unit tested parts but not verified

--- hektar/scripts/ir_error.py
+++ hektar/scripts/ir_error.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python
 import rospy
-from hektar.msg import array
-from std_msgs import Float64
+from hektar.msg import IRarray
+from std_msgs.msg import Float64
+
 
 # PARAMS and ASSUMPTIONS
 #* Input: array of IR sensor vals (0-1024)
@@ -9,13 +10,11 @@ from std_msgs import Float64
 #
 #* ASSUME setpoint is 0 (center)
 
-
-
-pub = rospy.Publisher('ir_correction', Float64, queue_size=1)
+pub = rospy.Publisher('state', Float64, queue_size=1)
 
 # Initilaize parameters
 
-def array_callback(msg){
+def array_callback(msg):
   sensor0 = 1023 - msg.ir_0
   sensor1 = 1023 - msg.ir_1
   sensor2 = 1023 - msg.ir_2
@@ -23,17 +22,15 @@ def array_callback(msg){
   sensor4 = 1023 - msg.ir_4
   
   pos = Float64()
-  pos.data = (sensor0 * -2 + sensor1 * -1 + sensor3 + sensor4 * 2) / (sensor0 + sensor1 + sensor2 + sensor3 + sensor4)
+  pos.data = (sensor0 * -2 + sensor1 * -1 + sensor3 + sensor4 * 2) / ( 1.0 * (sensor1 + sensor1 + sensor2 + sensor3 + sensor4) )
+  rospy.loginfo(rospy.get_caller_id() + " Error: %f", pos.data)
   pub.publish(pos)
-
-}
-
 
 
 def control():
   rospy.init_node('ir_error', anonymous=True)
 
-  rospy.Subscriber('ir_array', array, array_callback, queue_size=1, tcp_nodelay=False)   
+  rospy.Subscriber('ir_array', IRarray, array_callback, queue_size=1, tcp_nodelay=False)   
  
   rospy.spin()
 
---
+++ hektar/scripts/wheel_control.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+import rospy
+from hektar.msg import wheelVelocity 
+from std_msgs.msg import Float64
+
+speed = 70
+
+pub = rospy.Publisher('wheel_output', wheelVelocity, queue_size=1)
+
+# Initilaize parameters
+
+def wheel_callback(feedback):
+  wheels = wheelVelocity()
+  wheels.wheelL = speed + feedback.data 
+  wheels.wheelR = speed - feedback.data
+  rospy.loginfo(rospy.get_caller_id() + " Wheels: %d, %d", wheels.wheelL, wheels.wheelR)
+  pub.publish(wheels)
+
+
+def control():
+  rospy.init_node('wheel_control', anonymous=True)
+
+  rospy.Subscriber('control_effort', Float64, wheel_callback, queue_size=1, tcp_nodelay=False)   
+ 
+  rospy.spin()
+
+if __name__ == '__main__':
+  try:
+    control()
+  except rospy.ROSInterruptException: pass
+
