5dd2d1f38f0a5ca9937d2174df38de02ccb4998f
Fiona <fiona@example.com>
2019-07-17 16:17:11 -0700

    [Major] Made wheel_control.py node dynamically reconfigurable so that speed and variation_factor parameters can be manually edited on the fly.

--- hektar/scripts/wheel_control.py
+++ hektar/scripts/wheel_control.py
@@ -2,27 +2,41 @@
 import rospy
 from hektar.msg import wheelVelocity 
 from std_msgs.msg import Float64
+from dynamic_reconfigure.server import Server
+from hektar.cfg import HektarConfig
 
-speed = 70
 
-pub = rospy.Publisher('wheel_output', wheelVelocity, queue_size=1)
+class Callback():
+  def __init__(self):
+    self.speed = 70
+    self.variation_factor = 0.5
 
-# Initilaize parameters
+  def wheel_callback(self, feedback):
+    wheels = wheelVelocity()
+    wheels.wheelL = self.speed + (feedback.data * self.variation_factor) 
+    wheels.wheelR = self.speed - (feedback.data * self.variation_factor)
+    rospy.loginfo(rospy.get_caller_id() + " Wheels: %d, %d", wheels.wheelL, wheels.wheelR)
+    
+    pub = rospy.Publisher("wheel_output", wheelVelocity, queue_size=1)
+    pub.publish(wheels)
 
-def wheel_callback(feedback):
-  wheels = wheelVelocity()
-  wheels.wheelL = speed + feedback.data 
-  wheels.wheelR = speed - feedback.data
-  rospy.loginfo(rospy.get_caller_id() + " Wheels: %d, %d", wheels.wheelL, wheels.wheelR)
-  pub.publish(wheels)
+  # required to dynamically reconfigure parameters
+  def callback(self, config, level):
+    rospy.loginfo("""Reconfigure Request: {speed}, {variation_factor}""".format(**config))
+    self.speed = config["speed"]
+    self.variation_factor = config["variation_factor"]
+    return config
 
 
 def control():
   rospy.init_node('wheel_control', anonymous=True)
-
-  rospy.Subscriber('control_effort', Float64, wheel_callback, queue_size=1, tcp_nodelay=False)   
- 
+  
+  callbacker = Callback()
+  srv = Server(HektarConfig, callbacker.callback)
+  rospy.Subscriber('control_effort', Float64, callbacker.wheel_callback, queue_size=1, tcp_nodelay=False)   
+  
   rospy.spin()
+
 
 if __name__ == '__main__':
   try:
