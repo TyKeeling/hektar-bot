2c61392663d4b7d4af1e27457609a9018c840259
Tyler Keeling <tylerwkeeling@gmail.com>
2019-07-18 12:35:41 -0700

    [major] Fiona: added slider parameterization of wheel offset to help with tuning

--- hektar/scripts/wheel_control.py
+++ hektar/scripts/wheel_control.py
@@ -6,38 +6,42 @@ from std_msgs.msg import Float64
 from dynamic_reconfigure.server import Server
 from hektar.cfg import HektarConfig
 
-pub = rospy.Publisher("wheel_output", wheelVelocity, queue_size=1)
+
+UPPER_LIMIT = 127
+LOWER_LIMIT = -127
+
 
 class Callback():
   def __init__(self):
     self.speed = 0
     self.variation_factor = 0.0
+    self.offset_multiplier = 0.0
+    self.offset_addition = 0
+    self.pub = rospy.Publisher("wheel_output", wheelVelocity, queue_size=1)
 
   def wheel_callback(self, feedback):
     wheels = wheelVelocity()
-    a = self.speed + int(feedback.data * self.variation_factor) 
-    if a > 127:
-      a = 127
-    elif a < -127:
-      a = -127
-    wheels.wheelL = a
     
-    a = self.speed - int(feedback.data * self.variation_factor)                
-    if a > 127:
-      a = 127
-    elif a < -127:
-      a = -127
-    wheels.wheelR = a
+    # scalar and addition offsets for left wheel to account for wheel speed
+    # discrepancies
+    computed_speedL = int((self.speed + (feedback.data * self.variation_factor)) \
+      * self.offset_multiplier + self.offset_addition)
+    wheels.wheelL = max(LOWER_LIMIT, min(computed_speedL, UPPER_LIMIT))
+    
+    computed_speedR = int(self.speed - (feedback.data * self.variation_factor))               
+    wheels.wheelR = max(LOWER_LIMIT, min(computed_speedR, UPPER_LIMIT))
 
     rospy.loginfo(rospy.get_caller_id() + " Wheels: %f, %f", wheels.wheelL, wheels.wheelR)
-    
     pub.publish(wheels)
 
   # required to dynamically reconfigure parameters
   def callback(self, config, level):
-    rospy.loginfo("""Reconfigure Request: {speed}, {variation_factor}""".format(**config))
+    rospy.loginfo("""Reconfigure Request: {speed}, {variation_factor}, \
+      {offset_multiplier}, {offset_addition}""".format(**config))
     self.speed = config["speed"]
     self.variation_factor = config["variation_factor"]
+    self.offset_multiplier = config["offset_multiplier"]
+    self.offset_addition = config["offset_addition"]
     return config
 
 
@@ -46,7 +50,8 @@ def control():
   
   callbacker = Callback()
   srv = Server(HektarConfig, callbacker.callback)
-  rospy.Subscriber('control_effort', Float64, callbacker.wheel_callback, queue_size=1, tcp_nodelay=False)   
+  rospy.Subscriber('control_effort', Float64, callbacker.wheel_callback, \
+     queue_size=1, tcp_nodelay=False)   
   rospy.spin()
 
 
