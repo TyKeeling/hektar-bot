6919aef1f8448dd81bf5988199779dd15bc44760
Fiona <fiona@example.com>
2019-07-18 21:30:44 -0700

    [Major] added slider for ir_array threshold

--- hektar/scripts/ir_error.py
+++ hektar/scripts/ir_error.py
@@ -2,7 +2,8 @@
 import rospy
 from hektar.msg import IRarray
 from std_msgs.msg import Float64
-
+from dynamic_reconfigure.server import Server
+from hektar.cfg import HektarConfig
 
 # PARAMS and ASSUMPTIONS
 #* Input: array of IR sensor vals (0-1024)
@@ -10,40 +11,58 @@ from std_msgs.msg import Float64
 #
 #* ASSUME setpoint is 0 (center)
 
-THRESHOLD = 600
 
-pub = rospy.Publisher('state', Float64, queue_size=1)
-lastPos = Float64()
-lastPos.data = 0
+OFF_TAPE_ERROR = 6
 
 
-def array_callback(msg):
-  sensors = [msg.ir_0, msg.ir_1, msg.ir_2, msg.ir_3, msg.ir_4]
-  pos = Float64()
+class Ir_Error():
+  def __init__(self):
+    self.pub = rospy.Publisher('state', Float64, queue_size=1)
+    self.threshold = 0
+    self.lastPos = Float64()
+    self.lastPos.data = 0
 
-  if above_threshold(sensors):
-    if lastPos.data > 1.5: 
-        pos.data = 6
-    elif lastPos.data < -1.5:
-        pos.data = -6
-  else: pos.data = -sensors.index(min(sensors)) +2 
+  def array_callback(self, msg):
+    sensors = [msg.ir_0, msg.ir_1, msg.ir_2, msg.ir_3, msg.ir_4]
+    pos = Float64()
 
-  rospy.loginfo(rospy.get_caller_id() + " Error: %f", pos.data)
-  lastPos.data = pos.data
-  pub.publish(pos)
+    if self.all_sensors_off_tape(sensors):
+      if self.lastPos.data > 1.5: 
+        pos.data = OFF_TAPE_ERROR
 
-def above_threshold(sensors):
-  for sensor in sensors:
-    if sensor < THRESHOLD:
-      return False
-  return True
+      elif self.lastPos.data < -1.5:
+        pos.data = -OFF_TAPE_ERROR
+	  
+      else:
+        pos.data = 0
+
+    else:
+        pos.data = -sensors.index(min(sensors)) + 2 
+
+    rospy.loginfo(rospy.get_caller_id() + " Error: %f", pos.data)
+    self.lastPos.data = pos.data
+    self.pub.publish(pos)
+
+  def all_sensors_off_tape(self, sensors):
+    for sensor in sensors:
+      if sensor < self.threshold:
+        return False
+    return True
+
+  def callback(self, config, level):
+    rospy.loginfo("""Reconfigure Request: {threshold}""".format(**config))
+    self.threshold = config["threshold"]
+    return config
+
 
 def control():
   rospy.init_node('ir_error', anonymous=True)
-
-  rospy.Subscriber('ir_array', IRarray, array_callback, queue_size=1, tcp_nodelay=False)   
- 
+  
+  callbacker = Ir_Error()
+  srv = Server(HektarConfig, callbacker.callback)
+  rospy.Subscriber('ir_array', IRarray, callbacker.array_callback, queue_size=1, tcp_nodelay=False)   
   rospy.spin()
+
 
 if __name__ == '__main__':
   try:
