9d975529d7080e46cf7ad46c86e083d61087504b
Emma Gray <e.gray@alumni.ubc.ca>
2019-08-01 21:44:20 -0700

    [major] - keyboard control now prints location as well

--- hektar/src/keyboard_arm.py
+++ hektar/src/keyboard_arm.py
@@ -6,11 +6,18 @@
 from __future__ import print_function
 import rospy
 import roslib
+from hektar.msg import armPos
+import kinematics
+import math
+
 from hektar.msg import armCtrl
-from dynamic_reconfigure.server import Server
+
 import sys, select, termios, tty
 
 pub = rospy.Publisher('arm_commands', armCtrl, queue_size = 10)
+
+offsetShoulder = -254
+offsetElbow = -330
 
 settings = termios.tcgetattr(sys.stdin)
 
@@ -20,6 +27,7 @@ Reading from the keyboard  and Publishing to arm_commands
 Moving around:
     u    i 
  h   j    k    l
+
 anything else : stop
 CTRL-C to quit
 """
@@ -45,6 +53,41 @@ class Servo:
     self.angle = angle
 
       
+def location_callback(msg):
+
+  speed = 40
+
+  theta, r, z = kinematics.unsolve(0, math.pi - (msg.shoulderPos + offsetShoulder)/162.9, -(msg.elbowPos + offsetElbow)/162.9)
+  rospy.loginfo("Location: r: %d z: %d" % (r, z))
+  angles = [0,0,0]
+  kinematics.solve(float(0), float(r), float(z), angles)
+  newShoulder = -(angles[1]-math.pi)*162.9 - offsetShoulder
+  newElbow = (angles[2]*162.9) - offsetElbow
+  rospy.loginfo("Pot vals: shoulder: %d, elbow:%d" % (newShoulder, newElbow))
+  key = getKey()
+  if key in moveBindings.keys():
+       turn = moveBindings[key][0]
+       elbow = moveBindings[key][1]
+       shoulder = moveBindings[key][2]
+  else:
+       turn = 0
+       elbow = 0
+       shoulder = 0
+  msg = armCtrl()
+  msg.elbowVel = elbow*speed
+  msg.shoulderVel = shoulder*speed 
+  msg.baseVel = servo.getAngle() + (5*turn)
+  if (msg.baseVel > 90):
+    msg.baseVel = 90
+  elif (msg.baseVel < -90):
+    msg.baseVel = -90
+
+  servo.setAngle(int(msg.baseVel))
+  pub.publish(msg)
+
+ 
+  
+
 	
 def getKey():
     tty.setraw(sys.stdin.fileno())
@@ -54,59 +97,18 @@ def getKey():
     termios.tcsetattr(sys.stdin, termios.TCSADRAIN, settings)
     return key
 
-class Slider():
-  def __init__(self):
-    self.speed = 0
-  def callback(self, config, level):
-    rospy.loginfo("Reconfigure Request: {keyboard_speed}".format(**config))
-    self.speed = config["keyboard_speed"]
-    return config
-
-def control():
-    servo = Servo()
-    servo.setAngle(90)
-
-    rospy.loginfo("reset!")
-
-
-    turn = 0
-    elbow = 0
-    shoulder = 0
-
-    try:
-        while(1):
-            key = getKey()
-            if key in moveBindings.keys():
-                turn = moveBindings[key][0]
-                elbow = moveBindings[key][1]
-                shoulder = moveBindings[key][2]
-             
-            else:
-                turn = 0
-                elbow = 0
-                shoulder = 0
-                if (key == '\x03'):
-                    break
-            msg = armCtrl()
-            msg.elbowVel = elbow*speed
-            msg.shoulderVel = shoulder*speed 
-	    msg.baseVel = servo.getAngle() + (5*turn)
-         
-            servo.setAngle(int(msg.baseVel))
-          
-            pub.publish(msg)
-    except Exception as e:
-	rospy.loginfo(e)
-
-    rospy.spin()
   
 
 if __name__=="__main__":
-    try:
+      servo = Servo()
+      servo.setAngle(90)
+
       rospy.init_node('keyboard_arm', anonymous=True)
       termios.tcsetattr(sys.stdin, termios.TCSADRAIN, settings)
-      control()
+      while not rospy.is_shutdown():
+      	rospy.Subscriber('arm_positions', armPos, location_callback, queue_size=1, tcp_nodelay=False)
 
-    except rospy.ROSInterruptException: pass
+      rospy.spin()
+
 
 
